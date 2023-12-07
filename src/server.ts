c591616dd7fdaf11a8c6e16d27d58a9ad1012bf5
nikabilanych <nikabilanych@gmail.com>
2023-12-07 19:52:22 +0100

    init selfhosting option

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,18 @@
-//cms & admin dashboard
-//payload cms via https://payloadcms.com/docs/getting-started/installation
-
-
+//--> db client <--//
+//--> cms & admin dashboard <--//
+//payload cms via https://payloadcms.com/docs/getting-started/installationxw
 import { getPayloadClient } from "./get-payload";
 import express from "express"
 const app=express();
-const PORT=Number(process.env.PORT)||3000;
+const PORT = Number(process.env.PORT)||3000;
 const start = async () => {
-    const payload = await getPayloadClient();
+    const payload = await getPayloadClient({
+        initOptions: {
+            express: app,
+            onInit: async (cms) => {
+                cms.logger.info(`Payload Admin URL: ${cms.getAdminURL()}`); 
+            },
+        }
+    });
 };
 start()

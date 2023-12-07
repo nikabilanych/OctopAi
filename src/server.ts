019f3cb194e583e50948122bed770df235c495d0
nikabilanych <nikabilanych@gmail.com>
2023-12-07 19:57:09 +0100

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@
 //payload cms via https://payloadcms.com/docs/getting-started/installationxw
 import { getPayloadClient } from "./get-payload";
 import express from "express"
+import { nextHandler } from "./next-utils";
 const app=express();
 const PORT = Number(process.env.PORT)||3000;
 const start = async () => {
@@ -13,6 +14,9 @@ const start = async () => {
                 cms.logger.info(`Payload Admin URL: ${cms.getAdminURL()}`); 
             },
         }
-    });
+    })
+    // -> option for self host, no need for vercel --
+    app.use((req,res) => nextHandler(req, res))
+    // SELFHOSTING
 };
 start()

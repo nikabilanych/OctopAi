d1e8ab03abe97fec65fbc34d2dad082fe1e9d7ca
nikabilanych <nikabilanych@gmail.com>
2023-12-08 19:30:27 +0100

    cms admin dashboard

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
-await import("./src/env.js");
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: "http",
+				hostname: "localhost",
+			},
+			{
+				protocol: "https",
+				hostname: "digitalhippo-production.up.railway.app",
+			},
+		],
+	},
+};
 
-/** @type {import("next").NextConfig} */
-const config = {};
-
-export default config;
+module.exports = nextConfig;
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -26,7 +26,7 @@ export default buildConfig({
     },
     editor: slateEditor({}),
     db: mongooseAdapter({
-        url: process.env.DATABASE_URL!,
+        url: process.env.MONGODB_URL!,
     }),
     typescript: {
         outputFile: path.resolve(__dirname, 'payload-types.ts'),
